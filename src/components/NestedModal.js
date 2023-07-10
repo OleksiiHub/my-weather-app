ae5e43ebac01efebbbf45dd319349080723339c0
OLEKSII BUNTIKOV <githubbai@gmail.com>
2023-07-10 16:53:07 +0300

    populating a modal window

--- src/components/NestedModal.js
+++ src/components/NestedModal.js
@@ -6,83 +6,46 @@ import { useState } from "react";
 
 
 const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    pt: 2,
-    px: 4,
-    pb: 3,
-  };
-  
-  function ChildModal() {
-    const [open, setOpen] = useState(false);
-    const handleOpen = () => {
-      setOpen(true);
-    };
-    const handleClose = () => {
-      setOpen(false);
-    };
-  
-    return (
-      <>
-        <Button onClick={handleOpen}>Open Child Modal</Button>
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="child-modal-title"
-          aria-describedby="child-modal-description"
-        >
-          <Box sx={{ ...style, width: 200 }}>
-            <h2 id="child-modal-title">Text in a child modal</h2>
-            <p id="child-modal-description">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            </p>
-            <Button onClick={handleClose}>Close Child Modal</Button>
-          </Box>
-        </Modal>
-      </>
-    );
-  }
-  
-  export function NestedModal() {
-    const [open, setOpen] = useState(false);
-    const handleOpen = () => {
-      setOpen(true);
-    };
-    const handleClose = () => {
-      setOpen(false);
-    };
-  
-    return (
-      <div>
-        <Button onClick={handleOpen}>About Me</Button>
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="parent-modal-title"
-          aria-describedby="parent-modal-description"
-        >
-          <Box sx={{ ...style, width: 400 }}>
-            <h2 id="parent-modal-title">Text in a modal</h2>
-            <p id="parent-modal-description">
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </p>
-            <ChildModal />
-          </Box>
-        </Modal>
-      </div>
-    );
-  }
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
 
-// export function AboutProject() {
-//     return (
-//         <>
-//             <h1 className = "g_color">About Me</h1>
-//         </>
-//     )
-// }
+
+export function NestedModal() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>About Me</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="parent-modal-title"
+        aria-describedby="parent-modal-description"
+      >
+        <Box sx={{ ...style, width: 400 }}>
+          <h2 id="parent-modal-title">Описание</h2>
+          <p className='m-1' id="parent-modal-description">
+            Приложение "Weather App v.1.1.0", в котором показывает текущую погоду о введеном Вами городе, а так же некоторые метерологические данные на текущий момент, и показывает что лучше одеть.
+          </p>
+          <p >Buntikov Oleksii  2023</p>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
